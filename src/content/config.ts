49f7420da57f6899d378b5785738d68e1de8294f
tat22444 <tat22444@gmail.com>
2026-01-05 13:55:48 +0900

    new

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from 'astro:content';
+
+const worksCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  works: worksCollection,
+};
